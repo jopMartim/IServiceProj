5182216092b362031502d40e4f5fe3e40507db52
RafaelFurtadoMartins <67593264+RafaelFurtadoMartins@users.noreply.github.com>
2021-04-13 16:19:04 -0300

    1.6

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -8,13 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class LoginPage implements OnInit {
   email: string;
   senha: string;
-  
+
   constructor() { }
 
   ngOnInit() {}
     login(){
-      
+
     }
-  
+
 
 }
